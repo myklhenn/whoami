5712360bcedd53ec144af73cb3ba1e350b93b33f
Michael Henning <mykl951@gmail.com>
2019-01-29 16:14:02 -0800

    Refactored CSS

--- js/index.jsx
+++ js/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import '../style.css';
 import Layout from './components/Layout';
 
 render(<Layout />, document.getElementById('app'));
